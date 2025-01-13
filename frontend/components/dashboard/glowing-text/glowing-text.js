fe40a22e90bfae9780a5a880fed7aaa17e5ee398
Kenensh <ken158ken@gmail.com>
2025-01-13 23:10:30 +0800

    修改

--- frontend/components/dashboard/glowing-text/glowing-text.js
+++ frontend/components/dashboard/glowing-text/glowing-text.js
@@ -1,18 +1,24 @@
-import React from 'react'
+// glowing-text.js
+import React, { useEffect, useState } from 'react'
 import styles from './glowing-text.module.scss'
 
-const isClient = typeof window !== 'undefined'
+const GlowingText = ({ text, className }) => {
+  const [isClient, setIsClient] = useState(false)
 
-const GlowingText = ({ text }) => {
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   // 如果在伺服器端，返回基本版本
   if (!isClient) {
-    return <h1>{text}</h1>
+    return <h1 className={className}>{text}</h1>
   }
 
   return (
-    <div className={styles.glowing_txt}>
+    <div className={`${styles.glowing_txt} ${className}`}>
       <h1>
         {text.split(/(\s+)/).map((segment, segmentIndex) => {
+          // 處理空格
           if (/\s+/.test(segment)) {
             return (
               <span key={`space-${segmentIndex}`} className={styles.space}>
@@ -21,11 +27,15 @@ const GlowingText = ({ text }) => {
             )
           }
 
-          return segment
-            .split('')
-            .map((letter, letterIndex) => (
-              <span key={`${segmentIndex}-${letterIndex}`}>{letter}</span>
-            ))
+          // 處理文字
+          return segment.split('').map((letter, letterIndex) => (
+            <span 
+              key={`${segmentIndex}-${letterIndex}`}
+              className={styles.letter}
+            >
+              {letter}
+            </span>
+          ))
         })}
       </h1>
     </div>
