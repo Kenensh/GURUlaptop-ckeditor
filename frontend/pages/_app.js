1738f473309a4d647534e7cb83700c0faae98562
Kenensh <ken158ken@gmail.com>
2025-01-16 14:01:08 +0800

    _app.js defaute layout 修改

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,15 +1,65 @@
 import { useEffect } from 'react'
-import { useRouter } from 'next/router' // 新增
+import { useRouter } from 'next/router'
 
-// ... (其他 imports 保持不變)
+// 修正 DefaultLayout 的導入路徑
+import DefaultLayout from '@/components/layout/default-layout'
+
+//render 用
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// 樣式
+import '@/styles/globals.scss'
+import '@/styles/product.scss'
+import '@/styles/cart.scss'
+import '@/styles/loader.scss'
+import '@/styles/coupon.scss'
+import '@/styles/header.scss'
+import '@/styles/footer.scss'
+
+// 首頁
+import '@/styles/frontPage.scss'
+
+// 文章/部落格用 css
+import '@/styles/ArticleDetail.scss'
+import '@/styles/ArticleHomePage.scss'
+import '@/styles/BlogCreated.scss'
+import '@/styles/BlogDetail.scss'
+import '@/styles/BlogEdit.scss'
+import '@/styles/BlogHomePage.scss'
+import '@/styles/BlogUserOverview.scss'
+import 'animate.css'
+
+// 載入購物車context
+import { CartProvider } from '@/hooks/use-cart-state'
+// 載入認証用context
+import { AuthProvider } from '@/hooks/use-auth'
+// 載入動畫context
+import { LoaderProvider } from '@/hooks/use-loader'
+// 自訂用載入動畫元件
+import { LoadingSpinner } from '@/components/dashboard/loading-spinner'
+// event的scss
+import '../styles/event.scss'
+// eventdetail的scss
+import '../styles/eventDetail.scss'
+// evenRegistration的scss
+import '../styles/eventRegistration.scss'
+// group的scss
+import '../styles/group.scss'
+// groupCreat的scss
+import '../styles/groupCreat.scss'
+
+import { GroupAuthProvider } from '@/context/GroupAuthContext'
+
+//  新增加
+import { LoadingProviderAnimation } from '@/context/LoadingContext'
+import LoadingAnimation from '@/components/LoadingAnimation/LoadingAnimation'
 
 const isClient = typeof window !== 'undefined'
 
 export default function MyApp({ Component, pageProps }) {
-  const router = useRouter() // 新增
-  
+  const router = useRouter()
+
   useEffect(() => {
-    // 增加全局錯誤處理
     if (isClient) {
       window.onerror = function (msg, url, lineNo, columnNo, error) {
         console.log('Global Error:', {
@@ -22,7 +72,6 @@ export default function MyApp({ Component, pageProps }) {
         return false
       }
 
-      // 處理 Promise 錯誤
       window.onunhandledrejection = function (event) {
         console.log('Unhandled Promise Rejection:', event.reason)
       }
@@ -31,15 +80,15 @@ export default function MyApp({ Component, pageProps }) {
     import('bootstrap/dist/js/bootstrap')
   }, [])
 
-  // 優化 getLayout
-  const getLayout = Component.getLayout || ((page) => (
-    <DefaultLayout>{page}</DefaultLayout>
-  ))
-
-  // 檢查是否在服務器端渲染且是需要認證的頁面
-  if (!isClient) {
-    return getLayout(<Component {...pageProps} />)
-  }
+  const getLayout =
+    Component.getLayout ||
+    ((page) => {
+      if (!isClient) {
+        // SSR 基本渲染
+        return page
+      }
+      return <DefaultLayout>{page}</DefaultLayout>
+    })
 
   return (
     <AuthProvider>
